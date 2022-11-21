5dba173a6cf7a1b1169bfa894eeb52c21cb61177
GustavoR <rocha.gustavo.1am@gmail.com>
2022-11-21 17:40:29 -0600

    Agregar archivos de proyecto.

---
+++ ApiProyecto1002/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ApiProyecto1002.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiProyecto1002.Context
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<usuarios> usuarios { get; set; }
+    }
+}
